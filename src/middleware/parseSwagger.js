4d2894d8529b208eea6eb3492021700694679389
M0Bruno <bruno.maurice@matters.tech>
2019-07-05 16:34:08 +0200

    feat: refs #109868 add swagger middleware to access to a generated swagger documentation

---
+++ src/middleware/parseSwagger.js
@@ -0,0 +1,10 @@
+const ParseSwagger = require('../parse/parse-server-swagger');
+const { PARSE_APP_ID, PARSE_MASTER_KEY, PUBLIC_URL } = require('./../config');
+
+module.exports = new ParseSwagger({
+  host: PUBLIC_URL,
+  apiRoot: '/parse',
+  appId: PARSE_APP_ID,
+  masterKey: PARSE_MASTER_KEY,
+  excludes: ['_User', '_Role', '_Product', '_Session'],
+});
