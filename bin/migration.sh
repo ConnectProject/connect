232fe48c3eb9295f028b258714be0f200fdef811
Guillaume Bernos <guillaume.bernos@matters.tech>
2019-07-24 16:34:13 +0200

    feat: refs #110810 fix migration

--- bin/migration.sh
+++ bin/migration.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 source .env
-mongo --eval "var MONGO_HOST='$MONGO_HOST', MONGO_USERNAME='$MONGO_USERNAME', MONGO_PASSWORD='$MONGO_PASSWORD'" ./bin/migration_mongo.js
-LIST_COLLECTIONS=$(mongo connect --quiet --eval "printjson(db.getCollectionNames())" --username $MONGO_USERNAME --password $MONGO_PASSWORD)
+DOCKER_ID=$(docker ps --filter="name=connect_mongo_1" -q )
+
+docker cp ./bin/migration_mongo.js $DOCKER_ID:/migration_mongo.js
+docker cp ./bin/migration.js $DOCKER_ID:/migration.js
+
+docker exec $DOCKER_ID mongo --eval "var MONGO_HOST='$MONGO_HOST', MONGO_USERNAME='$MONGO_USERNAME', MONGO_PASSWORD='$MONGO_PASSWORD'" ./migration_mongo.js
+
+docker exec $DOCKER_ID mongo --eval "var MONGO_HOST='$MONGO_HOST', MONGO_USERNAME='$MONGO_USERNAME', MONGO_PASSWORD='$MONGO_PASSWORD'" ./bin/migration_mongo.js
+LIST_COLLECTIONS=$(docker exec $DOCKER_ID mongo connect --quiet --eval "printjson(db.getCollectionNames())" --username $MONGO_USERNAME --password $MONGO_PASSWORD)
 
 echo $LIST_COLLECTIONS
 node ./bin/migration.js "$LIST_COLLECTIONS"
