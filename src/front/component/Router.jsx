d9c80324ec532608b9e008b4b54779c40dad9295
Guillaume Bernos <guillaume.bernos@matters.tech>
2019-07-24 16:57:39 +0200

    feat: refs #110890 404 :sparkles:

--- src/front/component/Router.jsx
+++ src/front/component/Router.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import LoginPage from '../pages/login/login';
 import Github from '../pages/login/github';
 import HomePage from '../pages/home/home';
 import DetailsPage from '../pages/details/details';
 import ProfilePage from '../pages/profile/profile';
+import NoFound404Page from '../pages/notfound/notfound';
 
 export const ROUTES = {
   HOME: '/',
@@ -16,7 +17,7 @@ export const ROUTES = {
 class Routes extends React.PureComponent {
   render() {
     return (
-      <>
+      <Switch>
         <Route exact path="/login/github" component={Github} />
 
         <Route exact path="/application/:appId" component={DetailsPage} />
@@ -26,7 +27,9 @@ class Routes extends React.PureComponent {
         <Route exact path="/home" component={HomePage} />
 
         <Route exact path="/" component={LoginPage} />
-      </>
+
+        <Route component={NoFound404Page} />
+      </Switch>
     );
   }
 }
---
+++ src/front/pages/notfound/notfound.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable no-underscore-dangle */
+import * as React from 'react';
+import { withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import DeveloperModeIcon from '@material-ui/icons/DeveloperMode';
+
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+import PropTypes from 'prop-types'; // ES6
+
+const styles = {
+  card: {
+    maxWidth: 720,
+    margin: '0 auto',
+    marginTop: 120,
+  },
+};
+
+class NoFound404Page extends React.PureComponent {
+  async goToHome() {
+    const { history } = this.props;
+    console.log('home');
+    setTimeout(history.push(`/home`), 1000);
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            <DeveloperModeIcon />
+            {'  '} Page not Found
+          </Typography>
+          <Typography variant="body1" component="p">
+            It seems that you have landed here by mistake. No problem, you can
+            start from home again.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => this.goToHome()}>
+            Go to home
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+NoFound404Page.propTypes = {
+  classes: PropTypes.instanceOf(Object).isRequired,
+  history: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default withRouter(withStyles(styles)(NoFound404Page));
