358643a18ed6208195d4931e537bb1e4f614cd43
Guillaume Bernos <guillaume.bernos@matters.tech>
2019-07-24 16:34:50 +0200

    feat: refs #110810 fix sh file

--- bin/migration.sh
+++ bin/migration.sh
@@ -6,8 +6,6 @@ docker cp ./bin/migration_mongo.js $DOCKER_ID:/migration_mongo.js
 docker cp ./bin/migration.js $DOCKER_ID:/migration.js
 
 docker exec $DOCKER_ID mongo --eval "var MONGO_HOST='$MONGO_HOST', MONGO_USERNAME='$MONGO_USERNAME', MONGO_PASSWORD='$MONGO_PASSWORD'" ./migration_mongo.js
-
-docker exec $DOCKER_ID mongo --eval "var MONGO_HOST='$MONGO_HOST', MONGO_USERNAME='$MONGO_USERNAME', MONGO_PASSWORD='$MONGO_PASSWORD'" ./bin/migration_mongo.js
 LIST_COLLECTIONS=$(docker exec $DOCKER_ID mongo connect --quiet --eval "printjson(db.getCollectionNames())" --username $MONGO_USERNAME --password $MONGO_PASSWORD)
 
 echo $LIST_COLLECTIONS
