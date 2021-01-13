40510a407d5553a56b61013dd92de4f3c79eff75
Nicolas <nicolas@example.com>
2021-01-13 11:14:24 +0100

    merge api.spec.js and parse.spec.js and lint code

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,20 +1,24 @@
-/* eslint-disable */
 const axios = require('axios');
-const { bindAll, getJwtToken } = require(`${SPEC_PATH}/helper`);
+const { bindAll, getJwtToken } = require('./helper');
+const { API_URL } = require('./__mock__/config');
 
-process.env.TEST_SUITE = 'api-server';
+bindAll();
 
 describe('api server', () => {
-  bindAll();
-
+  let jwtToken;
   let applicationId;
 
-  it('Create Application', async () => {
-    const jwtToken = await getJwtToken();
+  it('Get JSON web token', async () => {
+    jwtToken = await getJwtToken();
 
+    expect(jwtToken).toBeDefined();
+    expect.assertions(1);
+  });
+
+  it('Create Application', async () => {
     const response = await axios({
       method: 'post',
-      url: 'http://localhost:3000/api/application',
+      url: `${API_URL}/api/application`,
       headers: {
         Authorization: `Bearer ${jwtToken}`,
         'Content-Type': 'application/json',
@@ -39,11 +43,9 @@ describe('api server', () => {
   });
 
   it('List Application', async () => {
-    const jwtToken = await getJwtToken();
-
     const response = await axios({
       method: 'get',
-      url: 'http://localhost:3000/api/application',
+      url: `${API_URL}/api/application`,
       headers: {
         Authorization: `Bearer ${jwtToken}`,
         'Content-Type': 'application/json',
@@ -55,11 +57,9 @@ describe('api server', () => {
   });
 
   it('Get Application', async () => {
-    const jwtToken = await getJwtToken();
-
     const getResponse = await axios({
       method: 'get',
-      url: `http://localhost:3000/api/application/${applicationId}`,
+      url: `${API_URL}/api/application/${applicationId}`,
       headers: {
         Authorization: `Bearer ${jwtToken}`,
         'Content-Type': 'application/json',
@@ -71,11 +71,9 @@ describe('api server', () => {
   });
 
   it('Update Application', async () => {
-    const jwtToken = await getJwtToken();
-
     const response = await axios({
       method: 'put',
-      url: `http://localhost:3000/api/application/${applicationId}`,
+      url: `${API_URL}/api/application/${applicationId}`,
       headers: {
         Authorization: `Bearer ${jwtToken}`,
         'Content-Type': 'application/json',
@@ -96,11 +94,9 @@ describe('api server', () => {
   });
 
   it('Delete Developer', async () => {
-    const jwtToken = await getJwtToken();
-
     const response = await axios({
       method: 'delete',
-      url: 'http://localhost:3000/api/developer',
+      url: `${API_URL}/api/developer`,
       headers: {
         Authorization: `Bearer ${jwtToken}`,
         'Content-Type': 'application/json',
@@ -112,5 +108,133 @@ describe('api server', () => {
     expect(response.data[1].deletedCount).toBe(1);
     expect(response.data[2].deletedCount).toBe(1);
     expect.assertions(4);
+  });
+});
+
+describe('parse custom server', () => {
+  let sessionToken;
+  let objectId;
+
+  it('Get session token', async () => {
+    const jwtToken = await getJwtToken();
+
+    const response = await axios({
+      method: 'post',
+      url: `${API_URL}/api/application`,
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        'Content-Type': 'application/json',
+      },
+      data: {
+        name: 'toto',
+        description: 'truc',
+        apple_store_link: 'https://apple.fr',
+        google_market_link: 'https://google.fr',
+      },
+    });
+
+    const parseResponse = await axios({
+      method: 'get',
+      url: `${API_URL}/parse/login`,
+      headers: {
+        'x-parse-application-id': `test`,
+        'x-parse-revocable-session': '1',
+      },
+      data: {
+        username: response.data.parse_name,
+        password: response.data.token,
+      },
+    });
+
+    expect(parseResponse.data.sessionToken).toBeDefined();
+    expect.assertions(1);
+
+    sessionToken = parseResponse.data.sessionToken;
+  });
+
+  it('Create Item', async () => {
+    const parseResponse = await axios({
+      method: 'post',
+      url: `${API_URL}/parse/classes/GameScore`,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-parse-application-id': `test`,
+        'x-parse-session-token': sessionToken,
+      },
+      data: {
+        score: 1337,
+        playerName: 'test9',
+        cheatMode: false,
+      },
+    });
+
+    expect(parseResponse.data.score).toBe(1337);
+    expect(parseResponse.data.playerName).toBe('test9');
+    expect(parseResponse.data.cheatMode).toBe(false);
+    expect(parseResponse.data.createdAt).toBeDefined();
+    expect(parseResponse.data.updatedAt).toBeDefined();
+    expect(parseResponse.data.objectId).toBeDefined();
+    expect(parseResponse.data.ACL).toBeUndefined();
+    expect(parseResponse.data.owner).toBeUndefined();
+    expect.assertions(8);
+
+    objectId = parseResponse.data.objectId;
+  });
+
+  it('Get Items', async () => {
+    const parseResponse = await axios({
+      method: 'get',
+      url: `${API_URL}/parse/classes/GameScore`,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-parse-application-id': `test`,
+        'x-parse-session-token': sessionToken,
+      },
+    });
+
+    expect(parseResponse.data.results.length).toBe(1);
+    expect(parseResponse.data.results[0].playerName).toBe('test9');
+    expect(parseResponse.data.results[0].objectId).toBe(objectId);
+    expect(parseResponse.data.results[0].ACL).toBeUndefined();
+    expect(parseResponse.data.results[0].owner).toBeUndefined();
+    expect.assertions(5);
+  });
+
+  it('Get Item', async () => {
+    const parseResponse = await axios({
+      method: 'get',
+      url: `${API_URL}/parse/classes/GameScore/${objectId}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-parse-application-id': `test`,
+        'x-parse-session-token': sessionToken,
+      },
+    });
+
+    expect(parseResponse.data.playerName).toBe('test9');
+    expect(parseResponse.data.objectId).toBe(objectId);
+    expect(parseResponse.data.ACL).toBeUndefined();
+    expect(parseResponse.data.owner).toBeUndefined();
+    expect.assertions(4);
+  });
+
+  it('Update Item', async () => {
+    const parseResponse = await axios({
+      method: 'put',
+      url: `${API_URL}/parse/classes/GameScore/${objectId}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-parse-application-id': `test`,
+        'x-parse-session-token': sessionToken,
+      },
+      data: {
+        cheatMode: true,
+      },
+    });
+
+    expect(parseResponse.data.cheatMode).toBe(true);
+    expect(parseResponse.data.ACL).toBeUndefined();
+    expect(parseResponse.data.owner).toBeUndefined();
+    expect.assertions(3);
   });
 });
