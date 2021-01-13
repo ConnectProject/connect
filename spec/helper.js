3599d66be715048f2367600c0a3506d0e429888e
Nicolas <nicolas@example.com>
2021-01-13 11:14:24 +0100

    merge api.spec.js and parse.spec.js and lint code

--- spec/helper.js
+++ spec/helper.js
@@ -1,121 +1,60 @@
-/* eslint-disable */
 const mongoose = require('mongoose');
 const nock = require('nock');
-const getClasses = require(`${SPEC_PATH}/../src/parse/schema/getClasses`);
 const { EventEmitter } = require('events');
 
 const configMock = require('./__mock__/config');
 
+configMock.MONGO_URI = 'mongodb://localhost:46347/connect';
+process.env = Object.assign(process.env, configMock);
+jest.mock('../src/config', () => configMock);
+
 // Set up a default API server for testing with default configuration.
-let ConnectServer;
-
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-
-const Parse = require('parse/node');
-Parse.serverURL = 'http://localhost:3000/parse';
+const ConnectServer = require('../src/connectServer');
+const AuthService = require('../src/api/services/auth');
 
 function bindServer() {
   let server;
-  let parse;
 
-  // Allows testing specific configurations of Parse Server
-  const reconfigureServer = () => {
-    return new Promise((resolve, reject) => {
-      if (server) {
-        return server.server.close(() => {
-          server = undefined;
-          reconfigureServer().then(resolve, reject);
-        });
-      }
-      try {
-        const event = new EventEmitter();
-        ConnectServer.start(configMock.APP_PORT, event).then(
-          (connectServer) => {
-            server = connectServer;
-            let expressFinish = false;
-            let parseInit = false;
-
-            event.once('parse-init', function (Parse) {
-              parse = Parse;
-              parseInit = true;
-              if (parseInit && expressFinish) {
-                resolve();
-              }
-            });
-
-            server.server.once('listening', function () {
-              expressFinish = true;
-
-              if (parseInit && expressFinish) {
-                resolve();
-              }
-            });
-          },
-        );
-      } catch (error) {
-        return reject(error);
-      }
-    });
-  };
-
-  beforeAll(async () => {
-    process.env.TESTING = true;
-    const connectionName = `mongodb://localhost:46347/${process.env.TEST_SUITE}`;
-    const parseConnect = mongoose.createConnection(connectionName);
-    const apiConnect = mongoose.createConnection(`${connectionName}-api`);
-    const parseSandboxConnect = mongoose.createConnection(
-      `${connectionName}-sandbox`,
-    );
-
-    jest.mock(`${SPEC_PATH}/../src/db/client`, () => {
-      return () => {
-        return {
-          parseConnect,
-          apiConnect,
-          parseSandboxConnect,
-        };
-      };
-    });
-    configMock.MONGO_URI = connectionName;
-    process.env = Object.assign(process.env, configMock);
-    jest.mock(`${SPEC_PATH}/../src/config`, () => configMock);
-
-    await getClasses();
-    ConnectServer = require('./../src/connectServer');
+  afterAll(async () => {
+    await mongoose.disconnect();
   });
 
-  afterAll(() => {
-    mongoose.disconnect().then(() => {
-      if (server) {
-        return server.server.close();
-      }
-    });
-  });
-
-  beforeEach(() => {
-    return reconfigureServer();
-  });
-
-  afterEach(async () => {
+  beforeAll(() => {
     return new Promise((resolve) => {
-      if (!server) {
-        resolve();
-      }
+      const event = new EventEmitter();
+      ConnectServer.start(configMock.APP_PORT, event).then((connectServer) => {
+        server = connectServer;
+        let expressFinish = false;
+        let parseInit = false;
 
-      parse.Cloud._removeAllHooks();
-      server.server.close(() => {
-        server = undefined;
-        resolve();
+        event.once('parse-init', () => {
+          parseInit = true;
+          if (parseInit && expressFinish) {
+            resolve();
+          }
+        });
+
+        server.server.once('listening', () => {
+          expressFinish = true;
+
+          if (parseInit && expressFinish) {
+            resolve();
+          }
+        });
       });
     });
+  });
+
+  afterAll(async () => {
+    if (server) {
+      await server.server.close();
+      server = undefined;
+    }
   });
 }
 
 function bindGithub() {
-  beforeEach(() => {
+  beforeAll(() => {
     nock(`https://github.com`)
       .persist()
       .post('/login/oauth/access_token')
@@ -131,7 +70,7 @@ function bindGithub() {
     });
   });
 
-  afterEach(() => nock.cleanAll());
+  afterAll(() => nock.cleanAll());
 }
 
 function bindAll() {
@@ -140,9 +79,8 @@ function bindAll() {
 }
 
 async function getJwtToken() {
-  const AuthService = require(`${SPEC_PATH}/../src/api/services/auth`);
   const authService = new AuthService();
-  return await authService.connectUser('lambda_github_code');
+  return authService.connectUser('lambda_github_code');
 }
 
 module.exports = {
