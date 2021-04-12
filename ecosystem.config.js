74428dace9c58efa8714d4b5601079e9772d8ee1
Nicolas T <ntraut9@gmail.com>
2021-04-12 16:30:48 +0200

    add node_modules and logs dirs to pm2 ignore_watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,13 @@ module.exports = {
       name: 'connect',
       script: 'src/index.js',
       watch: '.',
+      ignore_watch : ["node_modules", "logs"],
     },
     {
       name: 'connect-sandbox',
       script: 'src/sandbox.js',
       watch: '.',
+      ignore_watch : ["node_modules", "logs"],
     },
   ],
 };
