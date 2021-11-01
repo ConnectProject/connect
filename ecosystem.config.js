3565ea50ef6bf9171b7ebf26cce38b6d3ad514e8
Nicolas <ntraut9@gmail.com>
2021-11-01 12:32:09 +0100

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
       script: 'src/index.js',
       watch: '.',
       ignore_watch: ['node_modules', 'logs'],
+      exp_backoff_restart_delay: 100,
+      log_date_format: "YYYY-MM-DD HH:mm:ss.SSS",
     },
   ],
 };
